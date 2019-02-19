7e1cb18641ca9c1957250e750def57c0bee86e03
Michail Koroteev <sairos@bk.ru>
2019-02-19 17:00:36 +0300

    Add simple button

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.helloandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView mainTextView;
+    Button mainButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +18,13 @@ public class MainActivity extends AppCompatActivity {
 
         mainTextView = findViewById(R.id.main_textview);
         mainTextView.setText(getString(R.string.java_string));
+
+        mainButton = findViewById(R.id.main_button);
+        mainButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        mainTextView.setText(getString(R.string.text_after_button));
     }
 }
