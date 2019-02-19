e15c9153793b5cc3133ce6e451e0a724544912d4
Michail Koroteev <sairos@bk.ru>
2019-02-19 16:49:14 +0300

    refactor

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.helloandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mainTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainTextView = findViewById(R.id.main_textview);
+        mainTextView.setText(getString(R.string.java_string));
     }
 }
