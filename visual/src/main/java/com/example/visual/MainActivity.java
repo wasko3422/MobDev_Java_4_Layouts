22159ecba9d8e7ed6a3e3c72d73b2afdd74594e5
Michail Koroteev <sairos@bk.ru>
2019-03-05 12:05:39 +0300

    Created new model - visual element formatting

--- visual/src/main/java/com/example/visual/MainActivity.java
+++ visual/src/main/java/com/example/visual/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.visual;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout llBottom = (LinearLayout) findViewById(R.id.llBottom);
+        TextView tvBottom = (TextView) findViewById(R.id.tvBottom);
+        Button btnBottom = (Button) findViewById(R.id.btnBottom);
+
+        llBottom.setBackgroundResource(R.color.llBottomColor);
+        tvBottom.setText(R.string.tvBottomText);
+        btnBottom.setText(R.string.btnBottomText);
     }
 }
